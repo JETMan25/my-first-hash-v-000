94aa12bbcfcef77304386a835e6a93b4f80b1369
jontenthoff@hotmail.com <jontenthoff@hotmail.com>
2018-09-10 20:34:24 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -34,8 +34,8 @@ end
 def adding
   shipping_manifest{
     "whale bone corsets" => 5, 
-        "porcelain vases" => 2, 
-        "oil paintings" => 3
+    "porcelain vases" => 2, 
+    "oil paintings" => 3
   }
   
   shipping_manifest["muskets"] = 2 

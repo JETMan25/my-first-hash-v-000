4497c0ff1d137e6c1cf9456b0b1819dc3a836ed8
jontenthoff@hotmail.com <jontenthoff@hotmail.com>
2018-09-10 20:32:11 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -37,8 +37,8 @@ def adding
         "porcelain vases" => 2, 
         "oil paintings" => 3
   }
-  shipping_manifest[musk]
-end 
+  shipping_manifest["muskets"] = 2 
+
   # add 2 muskets to the shipping_manifest hash below
 
 

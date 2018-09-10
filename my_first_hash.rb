423c8dc6fd1ebee405f5d513352cbbbb2b0262ab
jontenthoff@hotmail.com <jontenthoff@hotmail.com>
2018-09-10 20:23:31 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,11 +1,6 @@
 def my_hash
- your_hash = {
-   "apple" => "a delicious fruit",
-   "friend" => "Nikki"
-  
-  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
-end
+ your_hash = {"name" => "Fred"}
+     # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
 end
 
 
